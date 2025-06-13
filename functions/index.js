6edf29fe9e809364c5287155f33cf469001d9060
Caleb Gomes Pinto <caleb.gp.dev@gmail.com>
2025-06-13 09:51:06 +0100

    feat: firebase functions adapted

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const { onRequest } = require("firebase-functions/v2/https");
 const { setupStreamHandler } = require("./handlers/setupStream");
 const { streamProxyHandler } = require("./handlers/streamProxy");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 app.get("/setup-stream/:type/:imdbId", setupStreamHandler);
 app.get("/stream/:identifier", streamProxyHandler);
@@ -14,9 +16,9 @@ app.get("/stream/:identifier", streamProxyHandler);
  */
 exports.api = onRequest(
   {
-    region: "us-central1",
-    timeoutSeconds: 300,
-    memory: "1GiB",
+    // region: "us-central1",
+    // timeoutSeconds: 300,
+    // memory: "1GiB",
   },
   app
 );
